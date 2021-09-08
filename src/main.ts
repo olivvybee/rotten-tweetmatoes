1d6f902a05e1c67f0547b70619c0364442c61c4b
Olivia Asch <olivia.c.asch@gmail.com>
2021-09-08 21:47:58 +0100

    Add CLI arguments for username and tweet count

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
 import { findMovieTweets } from './findMovieTweets';
 
 export const main = async () => {
-  const movieTweets = await findMovieTweets('olivvybee', 1000);
+  const argv = await yargs(hideBin(process.argv))
+    .option('username', {
+      alias: 'u',
+      type: 'string',
+      description: 'The username of the twitter user to analyse',
+    })
+    .option('count', {
+      alias: 'c',
+      type: 'number',
+      description: 'The total number of tweets to fetch to find movie tweets',
+      default: 250,
+    }).argv;
+
+  if (!argv.username) {
+    console.error('--username option is required.');
+    process.exit(1);
+  }
+
+  const movieTweets = await findMovieTweets(argv.username, argv.count);
   console.log(movieTweets);
 };
